c540979908703e5fc271a27703bdd308573d5a50
Khrystyna2 <l.xrustja@gmail.com>
2019-03-21 11:38:51 +0200

    added gift and scroll to section

--- js/main.js
+++ js/main.js
@@ -30,4 +30,41 @@ $(document).ready(function () {
         $('.top-menu').fadeToggle();
     });
 
+    // POPUP video-open start
+
+    var videoSrc;  
+    $('.open-popup').click(function() {
+        videoSrc = $(this).data( "src" );
+    });
+
+    $('.open-popup').click(function (event) {
+        event.preventDefault();
+        $('#overlay').fadeIn(400,
+            function () {
+                $('.popup-video-open')
+                    .css('display', 'block')
+                    .animate({ opacity: 1, top: '50%' }, 200);
+            });
+        $("#video").attr('src',videoSrc + "?autoplay=1" ); 
+    });
+    $('.modal_close, #overlay').click(function () {
+        $('.popup-video-open').animate({ opacity: 0, top: '45%' }, 200,
+            function () {
+                $(this).css('display', 'none');
+                $('#overlay').fadeOut(400);
+            }
+        );
+        $("#video").attr('src',videoSrc); 
+    });
+    // POPUP video-open end
+
+    // scroll section
+    let scrollLink = $('.scroll');
+    scrollLink.click(function(e) {
+        e.preventDefault();
+        $('body,html').animate({
+            scrollTop: $(this.hash).offset().top
+        }, 1000);
+    });
+
 });
